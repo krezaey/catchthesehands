c63e621c8248c591bcd175ddfc6a141d78d89f0a
kckc1289 <keziah.camille8@ymail.com>
2018-11-13 16:51:23 -0800

    Finished outline for JS game, need to implement moving parts

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let width = 960,
     coordinates = {
       section1: 330,
       section2: 530,
-    };
+    },
+    middle = (coordinates.section1 + coordinates.section2) / 2;
 
 
 function setup() {
@@ -18,21 +19,34 @@ function draw() {
 }
 
 function drawBackground()  {
+  strokeWeight(4);
   rect(0,0,coordinates.section1,height-1);
   rect(coordinates.section1,0,230,height-1);
   rect(coordinates.section2,0,coordinates.section1,height-1);
 }
 
 function drawHoop() {
-  rect(30,200,20,299);
-  rect(0,100,150,150);
-  ellipse(70,200,80,10);
+  strokeWeight(1);
+  //bar
+  rect(115,200,20,297);
+  //backboard
+  rect(50,100,150,150);
+  //rim
+  ellipse(125,200,80,10);
 }
 
 function drawBasketball() {
-  ellipse(width - 160, height - 60, 100);
+  strokeWeight(1);
+  let moveX = width - 160,
+      moveY = height - 60;
+  ellipse(moveX, moveY, 100);
 }
 
 function drawGame() {
-  ellise(coordinates.section2, 100, 50);
+  //game container
+  rect(middle - 20, 50, 40, 400);
+  //success bar
+  rect(middle - 20, 200, 40, 100);
+  //basketball icon
+  ellipse(middle, height - 250, 45);
 }
