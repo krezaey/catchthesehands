26ae6eb67141e840bc9d7012a5888ee0fdaf66c4
kckc1289 <keziah.camille8@ymail.com>
2018-11-14 23:17:27 -0800

    still figuring out p5 ... sigh

--- script.js
+++ script.js
@@ -1,58 +1,58 @@
 let width = 960,
-    height = 500,
-    coordinates = {
-      section1: 330,
-      midsec1: 330 / 2,
-      section2: 530,
-    },
-    middle = (coordinates.section1 + coordinates.section2) / 2;
+  height = 500,
+  coordinates = {
+    section1: 330,
+    midsec1: 330 / 2,
+    section2: 530
+  },
+  middle = (coordinates.section1 + coordinates.section2) / 2,
+  backgroundImage;
 
+function preload() {
+  let backgroundImage = loadImage("background.png");
 
+}
 function setup() {
-  createCanvas(width, height);
+  let canvas = createCanvas(width, height);
+  //image must be same length as the canvas
+  backgroundImage.loadPixels();
 }
 
 function draw() {
+  background(backgroundImage);
   drawBackground();
-  drawHoop();
   drawGame();
   drawBasketball();
 }
 
-function drawBackground()  {
-  rect(0,0,coordinates.section1,height-1);
-  rect(coordinates.section1,0,230,height-1);
-  rect(coordinates.section2,0,coordinates.section1,height-1);
-}
-
-function drawHoop() {
-  strokeWeight(1);
-  //bar
-  rect(coordinates.midsec1 - 20,200,20,297);
-  //backboard
-  rect(coordinates.midsec1 - 100, 100, 200, 150);
-  rect(coordinates.midsec1 - 50, 170, 100, 80);
-  //rim
-  ellipse(coordinates.midsec1,200,90,20);
-  //net
-  line(coordinates.midsec1 - 45, 200, coordinates.midsec1, 250);
-  line(coordinates.midsec1 + 45, 200, coordinates.midsec1, 250);
+function drawBackground() {
+  rect(0, 0, coordinates.section1, height - 1);
+  rect(coordinates.section1, 0, 230, height - 1);
+  rect(coordinates.section2, 0, coordinates.section1, height - 1);
 }
 
 function drawBasketball() {
-  strokeWeight(1);
   let moveX = width - 160,
-      moveY = height - 60;
+    moveY = height - 60;
   ellipse(moveX, moveY, 100);
   moveX = (moveX + 1) % coordinates.section3;
   moveY = (moveY + 1) % coordinates.section3;
 }
 
 function drawGame() {
-  //game container
-  rect(middle - 20, 50, 40, 400);
-  //success bar
-  rect(middle - 20, 200, 40, 100);
-  //basketball icon
-  ellipse(middle, height - 250, 50);
+  let max = 400,
+    min = 50,
+    upperY = 200,
+    lowerY = 300;
+ // while (ypos > min && ypos < max) {
+  document.addEventListener("keypress", function(event) {
+    if (event.key === "O") {
+      //movement up and down
+    }
+  });
+    //success bar
+    rect(middle - 20, 200, 40, 100);
+    //basketball icon
+    ellipse(middle, height - 250, 50);
+//  }
 }
